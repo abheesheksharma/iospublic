c7173cc25b11cda3b250ee43a9eff40475573024
abheesheksharma <koolabhishek837@gmail.com>
2019-08-28 16:08:00 +0530

    Create Map.swift

---
+++ Map.swift
@@ -0,0 +1,45 @@
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getCurrentLocation()
+    }
+    
+    func showMap(_ lat: Double, _ long: Double){
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let span = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Abhishek House"
+        
+        mapView.addAnnotation(annotation)
+    }
+    
+    func getCurrentLocation(){
+       
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+    }
+    
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue = manager.location?.coordinate else{
+            return
+        }
+            showMap(locValue.latitude, locValue.longitude)
+        
+    }
+    
+}
